4bac0ffa0b973427576dd00f8ec77314e5b9a3fc
Rodrigo Gomes da Silva <rodrigo.smscom@gmail.com>
2021-05-10 22:37:43 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# My personal crypto trading stuff
+
+#### Why trust ourselves our money? The machines will end up ruling anyway!  
+![](https://media1.giphy.com/media/50OuNGJcwIVXcuJURc/giphy.gif?cid=ecf05e47zl6yrmjhxbsj5kvl9208r3o0tpk5hdozk7aqe4e8&rid=giphy.gif)
+
+## Build
+#### Go ahead and bake the cake. This step can be ignored if you're using images in the docker-compose.yml file.
+```bash
+$ docker-compose build
+```
+
+## Run
+#### I just opened my eyes to crypto, so I'm still learning about all the stuff.
+#### The BBRSI strategy is already "kinda" optimized, but I'm currently working in a huge improvement in its parameters.
+#### The SMA strategy is not optimized at all, and the Hyperopt is not ready. But it's working...
+
+* This is not configured for dry run. By running the steps bellow, you're already trading.
+
+```bash
+# BBRSI
+$ docker-compose start freqtrade_bbrsi
+# SMA
+$ docker-compose start freqtrade_sma
+```
+
+## Download data
+#### This is pretty straight forward.
+#### You can pretty much download any chunk of time from your exchange for further machine learning and becktesting.
+#### That's it!
+```bash
+# BBRSI
+$ docker-compose run --rm freqtrade_bbrsi download-data --pairs-file ./user_data/data/binance/pairs_bbrsi.json --exchange binance -t 5m --days 90
+# SMA
+$ docker-compose run --rm freqtrade_sma download-data --pairs-file ./user_data/data/binance/pairs_sma.json --exchange binance -t 5m --days 90
+```
+
+## Backtesting
+#### The bot travels to past with information from the future and applies its knowledge to try to beat its own last record. Amazing!
+#### In truth, it just tests the strategy against the data you just downloaded from the above command...
+#### So it's still amazing, because you can improve your gorgeous strategy and test it with past data without even need to waste money.
+```bash
+# BBRSI
+$ docker-compose run --rm freqtrade_bbrsi backtesting --config ./user_data/config_bbrsi.json --strategy BBRSIOptimizedStrategy --datadir user_data/data/binance --export trades --max-open-trades=10 --stake-amount 26 -i 5m
+# SMA
+$ docker-compose run --rm freqtrade_sma backtesting --config ./user_data/config_sma.json --strategy SMAOffsetStrategy --datadir user_data/data/binance --export trades --max-open-trades=10 --stake-amount 26 -i 5m
+```
+
+## Train
+#### Not gonna give any explanation about this today...
+```bash
+# BBRSI
+$ docker-compose run --rm freqtrade_bbrsi hyperopt --config ./user_data/config_bbrsi.json --hyperopt BBRSIHyperopt --hyperopt-loss SortinoHyperOptLoss --strategy BBRSIOptimizedStrategy -i 5m --epochs 200 --stake-amount unlimited --min-trades 500 --max-open-trades 10
+# SMA
+$ docker-compose run --rm freqtrade_sma hyperopt --config ./user_data/config_sma.json --hyperopt SMAHyperopt --hyperopt-loss SortinoHyperOptLoss --strategy SMAOffsetOptimizedStrategy -i 5m --epochs 150 --stake-amount 26.6
+```
+
+## Plot
+#### This is not done, don't even try
+```
+# BBRSI
+$ docker-compose run --rm freqtrade_bbrsi hyperopt --config ./user_data/config_bbrsi.json --hyperopt BBRSIHyperopt --hyperopt-loss SortinoHyperOptLoss --strategy BBRSIOptimizedStrategy -i 5m --epochs 200 --stake-amount unlimited --min-trades 500 --max-open-trades 10
+# SMA
+$ docker-compose run --rm freqtrade_sma hyperopt --config ./user_data/config_sma.json --hyperopt SMAHyperopt --hyperopt-loss SortinoHyperOptLoss --strategy SMAOffsetOptimizedStrategy -i 5m --epochs 150 --stake-amount 26.6
+```
---
+++ user_data/strategies/sma_offset_strategy.py
@@ -0,0 +1,122 @@
+# --- Do not remove these libs ---
+from freqtrade.strategy.interface import IStrategy
+from typing import Dict, List
+from functools import reduce
+from pandas import DataFrame
+# --------------------------------
+
+import talib.abstract as ta
+import numpy as np
+import freqtrade.vendor.qtpylib.indicators as qtpylib
+import datetime
+from technical.util import resample_to_interval, resampled_merge
+from datetime import datetime, timedelta
+from freqtrade.persistence import Trade
+from freqtrade.strategy import stoploss_from_open, merge_informative_pair, DecimalParameter, IntParameter, CategoricalParameter
+
+SMA = 'SMA'
+EMA = 'EMA'
+
+# Buy hyperspace params:
+buy_params = {
+    "base_nb_candles_buy": 30,
+    "buy_trigger": SMA,
+    "low_offset": 0.92,
+}
+
+# Sell hyperspace params:
+sell_params = {
+    "base_nb_candles_sell": 41,
+    "high_offset": 1.026,
+    "sell_trigger": SMA,
+}
+
+class SMAOffsetStrategy(IStrategy):
+    INTERFACE_VERSION = 2
+
+    # ROI table:
+    minimal_roi = {"0": 1}
+
+    # Stoploss:
+    stoploss = -0.5
+
+    base_nb_candles_buy = IntParameter(5, 80, default=buy_params['base_nb_candles_buy'], space='buy')
+    base_nb_candles_sell = IntParameter(5, 80, default=sell_params['base_nb_candles_sell'], space='sell')
+    low_offset = DecimalParameter(0.9, 0.99, default=buy_params['low_offset'], space='buy')
+    high_offset = DecimalParameter(0.99, 1.1, default=sell_params['high_offset'], space='sell')
+    buy_trigger = CategoricalParameter([SMA, EMA], default=buy_params['buy_trigger'], space='buy')
+    sell_trigger = CategoricalParameter([SMA, EMA], default=sell_params['sell_trigger'], space='sell')
+
+    # Trailing stop:
+    trailing_stop = False
+    trailing_stop_positive = 0.1
+    trailing_stop_positive_offset = 0
+    trailing_only_offset_is_reached = False
+
+    # Optimal timeframe for the strategy
+    timeframe = '5m'
+
+    use_sell_signal = True
+    sell_profit_only = False
+
+    process_only_new_candles = True
+    startup_candle_count = 30
+
+    plot_config = {
+        'main_plot': {
+            'ma_offset_buy': {'color': 'orange'},
+            'ma_offset_sell': {'color': 'orange'},
+        },
+    }
+
+    use_custom_stoploss = False
+
+    def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        # uncomment for plotting
+
+        #if self.buy_trigger.value == 'EMA':
+        #    dataframe['ma_buy'] = ta.EMA(dataframe, timeperiod=self.base_nb_candles_buy.value)
+        #else:
+        #    dataframe['ma_buy'] = ta.SMA(dataframe, timeperiod=self.base_nb_candles_buy.value)
+
+        #if self.sell_trigger.value == 'EMA':
+        #    dataframe['ma_sell'] = ta.EMA(dataframe, timeperiod=self.base_nb_candles_sell.value)
+        #else:
+        #    dataframe['ma_sell'] = ta.SMA(dataframe, timeperiod=self.base_nb_candles_sell.value)
+
+        #dataframe['ma_offset_buy'] = dataframe['ma_buy'] * self.low_offset.value
+        #dataframe['ma_offset_sell'] = dataframe['ma_sell'] * self.high_offset.value
+
+        return dataframe
+
+    def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        if self.buy_trigger.value == EMA:
+            dataframe['ma_buy'] = ta.EMA(dataframe, timeperiod=int(self.base_nb_candles_buy.value))
+        else:
+            dataframe['ma_buy'] = ta.SMA(dataframe, timeperiod=int(self.base_nb_candles_buy.value))
+
+        dataframe['ma_offset_buy'] = dataframe['ma_buy'] * self.low_offset.value
+
+        dataframe.loc[
+            (
+                    (dataframe['close'] < dataframe['ma_offset_buy']) &
+                    (dataframe['volume'] > 0)
+            ),
+            'buy'] = 1
+        return dataframe
+
+    def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        if self.sell_trigger.value == EMA:
+            dataframe['ma_sell'] = ta.EMA(dataframe, timeperiod=int(self.base_nb_candles_sell.value))
+        else:
+            dataframe['ma_sell'] = ta.SMA(dataframe, timeperiod=int(self.base_nb_candles_sell.value))
+
+        dataframe['ma_offset_sell'] = dataframe['ma_sell'] * self.high_offset.value
+
+        dataframe.loc[
+            (
+                    (dataframe['close'] > dataframe['ma_offset_sell']) &
+                    (dataframe['volume'] > 0)
+            ),
+            'sell'] = 1
+        return dataframe
